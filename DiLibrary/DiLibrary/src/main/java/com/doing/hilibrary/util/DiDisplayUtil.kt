2ba358bd972ef169dd25749ba6444876f0fa7cb7
杜营 <duying@inke.cn>
2023-01-16 15:18:31 +0800

    ADD: 组织好工程基本结构

---
+++ DiLibrary/DiLibrary/src/main/java/com/doing/hilibrary/util/DiDisplayUtil.kt
@@ -0,0 +1,60 @@
+package com.doing.hilibrary.util
+
+import android.content.Context
+import android.content.res.Resources
+import android.graphics.Point
+import android.util.TypedValue
+import android.view.WindowManager
+
+object DiDisplayUtil {
+
+    fun dp2px(dp: Float, resources: Resources): Int {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, resources.displayMetrics)
+            .toInt()
+    }
+
+    fun dp2px(dp: Float): Int {
+        val resources: Resources = AppGlobals.get().resources
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, resources.displayMetrics)
+            .toInt()
+    }
+
+    fun sp2px(dp: Float): Int {
+        val resources: Resources = AppGlobals.get().resources
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, dp, resources.displayMetrics)
+            .toInt()
+    }
+
+
+    fun getDisplayWidthInPx(context: Context): Int {
+        val wm = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        if (wm != null) {
+            val display = wm.defaultDisplay
+            val size = Point()
+            display.getSize(size)
+            return size.x
+        }
+        return 0
+    }
+
+    fun getDisplayHeightInPx(context: Context): Int {
+        val wm = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        if (wm != null) {
+            val display = wm.defaultDisplay
+            val size = Point()
+            display.getSize(size)
+            return size.y
+        }
+        return 0
+    }
+
+    fun getStatusBarDimensionPx(): Int {
+        var statusBarHeight = 0
+        val res: Resources = AppGlobals.get().resources
+        val resourceId = res.getIdentifier("status_bar_height", "dimen", "android")
+        if (resourceId > 0) {
+            statusBarHeight = res.getDimensionPixelSize(resourceId)
+        }
+        return statusBarHeight
+    }
+}
