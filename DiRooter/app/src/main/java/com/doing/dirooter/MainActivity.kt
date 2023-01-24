6c20d159211448c893be3ded33904b64f543a922
MadProgramer <mengqidlufei8932@gmail.com>
2023-01-24 22:12:09 +0800

    [0.1.1 基础UI] ADD: 新增Rooter Module

---
+++ DiRooter/app/src/main/java/com/doing/dirooter/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.doing.dirooter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
