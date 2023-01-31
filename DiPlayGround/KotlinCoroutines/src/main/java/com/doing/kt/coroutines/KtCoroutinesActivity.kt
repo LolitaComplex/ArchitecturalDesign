a65bac465a0a2ca59017d9c8b4e47da0c656125d
MadProgramer <mengqidlufei8932@gmail.com>
2023-01-31 12:06:22 +0800

    [0.3.1 数据模块] ADD: 新增Kotlin携程测试代码模块

---
+++ DiPlayGround/KotlinCoroutines/src/main/java/com/doing/kt/coroutines/KtCoroutinesActivity.kt
@@ -0,0 +1,14 @@
+package com.doing.kt.coroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.doing.kt.coroutline.R
+
+class KtCoroutinesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kt_coroutines)
+
+
+    }
+}
